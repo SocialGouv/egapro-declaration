ca62c7f05f7616565c94b09cc6f7c9b89b3c872c
Mathieu Agopian <mathieu@agopian.info>
2020-11-23 08:22:43 +0100

    If indicateurs.rémunérations.mode is "autre" then next step is remuneration-coef.html

--- src/assets/js/tunnel.js
+++ src/assets/js/tunnel.js
@@ -14,6 +14,7 @@ const steps = [
   {name: 'entreprise', nextStep: _ => 'remuneration'},
   {name: 'remuneration', nextStep: data => {
     if (getVal(data, 'indicateurs.rémunérations.mode') === "coef") return 'remuneration-coef'
+    if (getVal(data, 'indicateurs.rémunérations.mode') === "autre") return 'remuneration-coef'
     if (getVal(data, 'indicateurs.rémunérations.mode') === "csp") return 'remuneration-csp'
     return 'augmentation'
   }},
