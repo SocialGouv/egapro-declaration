f191746586ad48bea2c44810bd150ed9b95a88e6
Mathieu Agopian <mathieu@agopian.info>
2020-12-03 15:44:07 +0100

    Publishing

--- assets/js/tunnel.js
+++ assets/js/tunnel.js
@@ -1,242 +1,262 @@
-const form = document.getElementById('page-form')
-const progress = document.querySelector('progress')
-const previousButton = document.querySelector('a[rel=prev]')
-const nextButton = document.querySelector('button[rel=next]')
+const form = document.getElementById("page-form");
+const progress = document.querySelector("progress");
+const previousButton = document.querySelector("a[rel=prev]");
+const nextButton = document.querySelector("button[rel=next]");
 const steps = [
-   {name: 'commencer'},
-   {name: 'declarant'},
-   {name: 'annee', nextStep: data => {
-    if(data._entreprise.structure === 'ues') return 'ues'
-    return 'entreprise'
-  }},
-  {name: 'ues'},
-  {name: 'ues-composition', nextStep: _ => 'remuneration'},
-  {name: 'entreprise', nextStep: _ => 'remuneration'},
-  {name: 'remuneration', nextStep: data => {
-    if (data.indicateurs.rémunérations.mode === "niveau_branche") return 'remuneration-coef'
-    if (data.indicateurs.rémunérations.mode === "niveau_autre") return 'remuneration-coef'
-    if (data.indicateurs.rémunérations.mode === "csp") return 'remuneration-csp'
-    return data.entreprise.effectif.tranche === '50:250' ? 'augmentation-et-promotion' : 'augmentation'
-  }},
-  {name: 'remuneration-coef', nextStep: _ => 'remuneration-final'},
-  {name: 'remuneration-autre', nextStep: _ => 'remuneration-final'},
-  {name: 'remuneration-csp', nextStep: _ => 'remuneration-final'},
-  {name: 'remuneration-final', nextStep: data => data.entreprise.effectif.tranche === '50:250' ? 'augmentation-et-promotion' : 'augmentation'
+  { name: "commencer" },
+  { name: "declarant" },
+  {
+    name: "annee",
+    nextStep: (data) => {
+      if (data._entreprise.structure === "ues") return "ues";
+      return "entreprise";
+    },
+  },
+  { name: "ues" },
+  { name: "ues-composition", nextStep: (_) => "remuneration" },
+  { name: "entreprise", nextStep: (_) => "remuneration" },
+  {
+    name: "remuneration",
+    nextStep: (data) => {
+      if (data.indicateurs.rémunérations.mode === "niveau_branche")
+        return "remuneration-coef";
+      if (data.indicateurs.rémunérations.mode === "niveau_autre")
+        return "remuneration-coef";
+      if (data.indicateurs.rémunérations.mode === "csp")
+        return "remuneration-csp";
+      return data.entreprise.effectif.tranche === "50:250"
+        ? "augmentations-et-promotions"
+        : "augmentations";
+    },
+  },
+  { name: "remuneration-coef", nextStep: (_) => "remuneration-final" },
+  { name: "remuneration-autre", nextStep: (_) => "remuneration-final" },
+  { name: "remuneration-csp", nextStep: (_) => "remuneration-final" },
+  {
+    name: "remuneration-final",
+    nextStep: (data) =>
+      data.entreprise.effectif.tranche === "50:250"
+        ? "augmentations-et-promotions"
+        : "augmentations",
   },
   // If tranche effectif is > 50:250
-  {name: 'augmentation'},
-  {name: 'promotion', nextStep: _ => 'maternite'},
+  { name: "augmentations" },
+  { name: "promotions", nextStep: (_) => "maternite" },
   // If tranche effectif is 50:250
-  {name: 'augmentation-et-promotion'},
+  { name: "augmentations-et-promotions" },
   //
-  {name: 'maternite'},
-  {name: 'hautesremunerations'},
-  {name: 'note'},
-  {name: 'resultat'},
-  {name: 'validation'},
-  {name: 'transmission'}
-]
+  { name: "maternite" },
+  { name: "hautes-remunerations" },
+  { name: "note" },
+  { name: "resultat" },
+  { name: "validation" },
+  { name: "transmission" },
+];
 
-const fileName = location.pathname.lastIndexOf('/') < 0 ? location.pathname : location.pathname.split('/').pop()
-const pageName = fileName.split('.')[0]
-const step = steps.findIndex(step => step.name === pageName)
+const fileName =
+  location.pathname.lastIndexOf("/") < 0
+    ? location.pathname
+    : location.pathname.split("/").pop();
+const pageName = fileName.split(".")[0];
+const step = steps.findIndex((step) => step.name === pageName);
 
+document.addEventListener("ready", () => {
+  if (!app.token) location.href = "/";
+  loadFormValues(form, app.data);
+});
 
-document.addEventListener('ready', () => {
-  if(!app.token) location.href = '/'
-  loadFormValues(form, app.data)
-})
-
-progress.max = steps.length - 1
-progress.value = step
+progress.max = steps.length - 1;
+progress.value = step;
 
 const saveFormData = async (event) => {
-  event && event.preventDefault()
+  event && event.preventDefault();
 
-  const data = serializeForm(form)
+  const data = serializeForm(form);
 
-  if(typeof document.onsend === 'function') {
+  if (typeof document.onsend === "function") {
     try {
-      await document.onsend(data)
-    } catch(e) {
-      return alert(e)
+      await document.onsend(data);
+    } catch (e) {
+      return alert(e);
     }
   }
 
-  return await app.save(data)
-}
+  return await app.save(data);
+};
 
-form.addEventListener('submit', async (event) => {
-  const response = await saveFormData(event)
+form.addEventListener("submit", async (event) => {
+  const response = await saveFormData(event);
 
-  if (!response.ok) return 
+  if (!response.ok) return;
 
-  const nextStep = steps[step].nextStep
-  if (nextStep) return redirect(`${nextStep(app.data)}.html`)
-  else return redirect(`${steps[step + 1].name}.html`)
-})
+  const nextStep = steps[step].nextStep;
+  if (nextStep) return redirect(`${nextStep(app.data)}.html`);
+  else return redirect(`${steps[step + 1].name}.html`);
+});
 
 const refreshForm = async () => {
-  let response = await saveFormData()
+  let response = await saveFormData();
 
-  if (!response.ok) return 
+  if (!response.ok) return;
 
-  response = await app.loadRemoteData()
-  if (!response.ok) return
-  loadFormValues(form, app.data)
-}
+  response = await app.loadRemoteData();
+  if (!response.ok) return;
+  loadFormValues(form, app.data);
+};
 
 // "Previous" button
 if (step > 0) {
   previousButton.onclick = (e) => {
-    e.preventDefault()
-    history.back()
-  }
-}
-else {
-  previousButton.setAttribute('disabled', 'disabled')
+    e.preventDefault();
+    history.back();
+  };
+} else {
+  previousButton.setAttribute("disabled", "disabled");
 }
 
 // "Next" button
-if(step >= steps.length - 1) {
-  nextButton.setAttribute('disabled', 'disabled')
+if (step >= steps.length - 1) {
+  nextButton.setAttribute("disabled", "disabled");
 }
 
 function serializeForm(form) {
-  let data = app.data
+  let data = app.data;
 
   // Only the fields that have names are of interest for us (not submit buttons)
-  const allFields = Array.from(form.elements).filter(field => field.name)
+  const allFields = Array.from(form.elements).filter((field) => field.name);
 
-  const formData = new FormData(form)
+  const formData = new FormData(form);
   formData.forEach((value, key) => {
     if (!value) {
-      return
+      return;
     }
-    const field = allFields.find(node => node.name === key)
-    if (field.type === 'number') {
-      value = Number(value)
+    const field = allFields.find((node) => node.name === key);
+    if (field.getAttribute("type") === "number") {
+      value = Number(value);
     }
-    setVal(data, key, value)
-  })
+    setVal(data, key, value);
+  });
 
   // Get all the names of the fields that aren't disabled (and thus included in FormData)
-  const enabledFields = Array.from(formData).map(formDataItem => formDataItem[0])
+  const enabledFields = Array.from(formData).map(
+    (formDataItem) => formDataItem[0]
+  );
 
   // We need to force remove disabled fields that might have been set previously
-  allFields.forEach(field => {
+  allFields.forEach((field) => {
     if (!enabledFields.includes(field.name) || field.value === "") {
-      delVal(app.data, field.name)
+      delVal(app.data, field.name);
     }
-  })
-  removeEmpty(data)
-  return data
+  });
+  removeEmpty(data);
+  return data;
 }
 
 function removeEmpty(data) {
-  Object.keys(data).forEach(key => {
+  Object.keys(data).forEach((key) => {
     if (typeof data[key] === "array") {
-      if (!data[key].filter(item => item !== undefined).length) {
-        delete data[key]
+      if (!data[key].filter((item) => item !== undefined).length) {
+        delete data[key];
       } else {
-        removeEmpty(data[key])
+        removeEmpty(data[key]);
       }
     } else if (typeof data[key] === "object") {
       if (!Object.keys(data[key]).length) {
-        delete data[key]
+        delete data[key];
       } else {
-        removeEmpty(data[key])
+        removeEmpty(data[key]);
       }
     }
-  })
+  });
 }
 
 function loadFormValues(form, data = {}) {
-  Array.from(form.elements).forEach(node => {
-    if (!node.name) return
-    const value = getVal(data, node.name)
-    if (value === "") return
+  Array.from(form.elements).forEach((node) => {
+    if (!node.name) return;
+    const value = getVal(data, node.name);
+    if (value === "") return;
     if (node.type === "radio") {
-      node.checked = node.value === value
+      node.checked = node.value === value;
     } else {
       // If it's a hidden input, it's one we generated, we don't want to overwrite it with an empty value
-      node.value = node.type === "hidden" ? node.value : value
+      node.value = node.type === "hidden" ? node.value : value;
     }
-  })
+  });
 }
 
 function extractKey(flatKey) {
-    // This extracts "foobar[0]" into ["foobar[0]", "foobar", "0"]
-    return flatKey.match(/([^\[]+)\[?(\d+)?\]?/)
+  // This extracts "foobar[0]" into ["foobar[0]", "foobar", "0"]
+  return flatKey.match(/([^\[]+)\[?(\d+)?\]?/);
 }
 
 function getVal(data, flatKey) {
-  const keys = flatKey.split('.')
+  const keys = flatKey.split(".");
   try {
     const value = keys.reduce((item, currentKey) => {
-      const [_, key, index] = extractKey(currentKey)
-      return index ? item[key][index] : item[key]
-    }, data)
-    return (value !== undefined ? value : '')
+      const [_, key, index] = extractKey(currentKey);
+      return index ? item[key][index] : item[key];
+    }, data);
+    return value !== undefined ? value : "";
   } catch {
     // Fail silently if the item doesn't exist yet
-    return ''
+    return "";
   }
 }
 
 function setVal(data, flatKey, val) {
   // Deeply set a value in data given a flatKey like `entreprise.ues.entreprises[0].raison_sociale`
-  const keys = flatKey.split('.')
-  const ancestors = keys.slice(0, -1)
-  const property = keys.pop()
+  const keys = flatKey.split(".");
+  const ancestors = keys.slice(0, -1);
+  const property = keys.pop();
 
   const target = ancestors.reduce((parent, name) => {
-    const [_, key, index] = extractKey(name)
+    const [_, key, index] = extractKey(name);
     // parent is an array
-    if(index) {
-      if(!(key in parent)) parent[key] = []
-      if(!parent[key][index]) parent[key][index] = {}
-      return parent[key][index]
+    if (index) {
+      if (!(key in parent)) parent[key] = [];
+      if (!parent[key][index]) parent[key][index] = {};
+      return parent[key][index];
     }
     // parent is an object
     else {
-      if(!(key in parent)) parent[key] = {}
-      return parent[key]
+      if (!(key in parent)) parent[key] = {};
+      return parent[key];
     }
-  }, data)
+  }, data);
 
   // Set the value on the item
-  const [_, key, index] = extractKey(property)
+  const [_, key, index] = extractKey(property);
   if (index) {
-    if(!(key in target)) target[key] = []
-    target[key][index] = val
+    if (!(key in target)) target[key] = [];
+    target[key][index] = val;
   } else {
-    target[key] = val
+    target[key] = val;
   }
 }
 
 function delVal(data, flatKey) {
   // Delete a nested value from a flat key
-  const keys = flatKey.split('.')
-  let item = data
+  const keys = flatKey.split(".");
+  let item = data;
   while (keys.length > 1) {
-    const [_, key, index] = extractKey(keys.shift())
-    if (!(key in item)) { // This item doesn't exist yet
-      return
+    const [_, key, index] = extractKey(keys.shift());
+    if (!(key in item)) {
+      // This item doesn't exist yet
+      return;
     }
     if (index && !item[key][index]) {
-      return
+      return;
     }
-    item = index ? item[key][index] : item[key]
+    item = index ? item[key][index] : item[key];
   }
   // Only one key left, it's the one that identifies the item we want to delete
-  const [_, key, index] = extractKey(keys.shift())
+  const [_, key, index] = extractKey(keys.shift());
   if (!(key in item)) {
-    return
+    return;
   }
   if (index) {
-    delete item[key][index]
+    delete item[key][index];
   } else {
-    delete item[key]
+    delete item[key];
   }
 }
