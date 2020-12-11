61ce5687544cfa61608e3dc11bb88c5f1e1bd30b
Mathieu Agopian <mathieu@agopian.info>
2020-12-11 16:05:20 +0100

    Check if a Siren is valid

--- src/assets/js/utils.js
+++ src/assets/js/utils.js
@@ -86,6 +86,34 @@ validateNotAllEmpty = event => {
   return true
 }
 
+isValidSiren = value => {
+  // https://fr.wikipedia.org/wiki/Syst%C3%A8me_d%27identification_du_r%C3%A9pertoire_des_entreprises#Calcul_et_validit%C3%A9_d'un_num%C3%A9ro_SIREN[3]
+  if (Number.isNaN(Number(value))) {
+    // Doit être un nombre ...
+    return false;
+  }
+  if (value.length !== 9) {
+    // ... composé de 9 chiffres.
+    return false;
+  }
+  const digits = value.split("").map(Number);
+  // Appliquer la formule de luhn à chaque digit en position impaire : https://fr.wikipedia.org/wiki/Formule_de_Luhn
+  const luhn = digits.map((digit, index) => {
+    if (index % 2) {
+      const doubled = digit * 2;
+      if (doubled >= 10) {
+        return doubled - 9;
+      }
+      return doubled;
+    }
+    return digit;
+  });
+  let sum = 0;
+  for (let i = 0; i < luhn.length; i++) {
+    sum += luhn[i];
+  }
+  return sum % 10 === 0;
+};
 
 class AppStorage {
   constructor() {
